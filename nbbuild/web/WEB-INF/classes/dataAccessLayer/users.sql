e8fd13ab3a9b8d5093a6ec742152d1c2d459e3ef
Raghav Chhabra <chha0030@algonquinlive.com>
2024-08-07 15:47:14 -0400

    sql queries added

---
+++ nbbuild/web/WEB-INF/classes/dataAccessLayer/users.sql
@@ -0,0 +1,16 @@
+-- Create the database
+CREATE DATABASE FoodWasteReduction;
+
+-- Switch to the new database
+USE FoodWasteReduction;
+
+-- Create the Users table
+CREATE TABLE Users (
+    userid INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    usertype VARCHAR(50) NOT NULL
+);
+
+select * from users;
