99235d180b0582da22dbb99fce5e4e865579ae9a
Sukhpreet <psukh4425@gmail.com>
2024-08-08 20:05:12 -0400

    Sukhpreet - UserSubscriptionDTO.java

---
+++ src/main/java/transferObject/UserSubscriptionDTO.java
@@ -0,0 +1,59 @@
+package transferObject;
+
+public class UserSubscriptionDTO {
+    private int id;
+    private String userEmail;
+    private String userPhone;
+    private String location;
+    private String foodPreferences;
+    private String communicationMethod; // "email" or "phone"
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getFoodPreferences() {
+        return foodPreferences;
+    }
+
+    public void setFoodPreferences(String foodPreferences) {
+        this.foodPreferences = foodPreferences;
+    }
+
+    public String getCommunicationMethod() {
+        return communicationMethod;
+    }
+
+    public void setCommunicationMethod(String communicationMethod) {
+        this.communicationMethod = communicationMethod;
+    }
+}
